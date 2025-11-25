ad882604872405eaf5c6c1249f84c57e9df3c256
Nico <nicolas.tome.38@gmail.com>
2025-11-25 15:57:21 +0100

    fix + improve bonus

--- so_long/src_bonus/parser/ft_check_map_bonus.c
+++ so_long/src_bonus/parser/ft_check_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ntome <ntome@42angouleme.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 22:41:26 by ntome             #+#    #+#             */
-/*   Updated: 2025/11/20 14:12:46 by ntome            ###   ########.fr       */
+/*   Updated: 2025/11/25 15:55:13 by ntome            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,41 @@ t_tiles_count	ft_init_tiles_count(void)
 	return (tiles_count);
 }
 
+t_vec2	ft_get_spawn(t_map map)
+{
+	t_vec2	read;
+
+	read.y = 0;
+	while (read.y < map.size)
+	{
+		read.x = 0;
+		while (map.map[read.y][read.x])
+		{
+			if (map.map[read.y][read.x] == SPAWN_TILE)
+				return (read);
+			read.x++;
+		}
+		read.y++;
+	}
+	return (read);
+}
+
+int	ft_check_monster_spawn(t_map map)
+{
+	t_vec2	spawn;
+
+	spawn = ft_get_spawn(map);
+	if (ft_strchr(ENEMYSET, map.map[spawn.y][spawn.x + 1]))
+		return (MAP_IMPOSSIBLE_MAP_ERROR_CODE);
+	if (ft_strchr(ENEMYSET, map.map[spawn.y][spawn.x - 1]))
+		return (MAP_IMPOSSIBLE_MAP_ERROR_CODE);
+	if (ft_strchr(ENEMYSET, map.map[spawn.y + 1][spawn.x]))
+		return (MAP_IMPOSSIBLE_MAP_ERROR_CODE);
+	if (ft_strchr(ENEMYSET, map.map[spawn.y - 1][spawn.x]))
+		return (MAP_IMPOSSIBLE_MAP_ERROR_CODE);
+	return (MAP_OK);
+}
+
 int	ft_check_map(t_map map)
 {
 	t_parser_infos	map_infos;
@@ -39,5 +74,6 @@ int	ft_check_map(t_map map)
 		error_code = ft_check_pathfinding(map);
 	free(map_infos.row_size);
 	free(map_infos.row_tiles);
+	error_code = ft_check_monster_spawn(map);
 	return (error_code);
 }
