37212d933b7d580516d2cba2f67c03140d187e14
nico-tome <nicolas.tome.38@gmail.com>
2026-03-13 15:06:00 +0100

    fix cpp7

--- cpp/cpp07/ex01/includes/iter.hpp
+++ cpp/cpp07/ex01/includes/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntome <ntome@42angouleme.fr>               +#+  +:+       +#+        */
+/*   By: ntome <ntome@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 18:31:46 by ntome             #+#    #+#             */
-/*   Updated: 2026/03/07 18:44:10 by ntome            ###   ########.fr       */
+/*   Updated: 2026/03/13 15:05:03 by ntome            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 #include <iostream>
 
-template <typename T>
-void iter(T *array, int size, void (*f)(T &))
+template <typename T, typename F>
+void iter(T *array, const int size, void (*f)(F &))
 {
 	for (int i = 0; i < size; i++)
 		f(array[i]);
