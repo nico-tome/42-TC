44a5e6af67bd1bd19731df8467052c0c20865c72
nico-tome <nicolas.tome.38@gmail.com>
2026-03-27 13:14:11 +0100

    finish cpp09

--- cpp/cpp09/ex02/includes/PmergeMe.hpp
+++ cpp/cpp09/ex02/includes/PmergeMe.hpp
@@ -6,16 +6,181 @@
 /*   By: ntome <ntome@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 18:29:31 by ntome             #+#    #+#             */
-/*   Updated: 2026/03/26 19:04:35 by ntome            ###   ########.fr       */
+/*   Updated: 2026/03/27 13:02:43 by ntome            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+# include <vector>
+# include <iostream>
+# include <algorithm>
+
+template <typename T>
 class PmergeMe
 {
+private:
+	T _container;
+
+public:
+	PmergeMe(void);
+	PmergeMe(const PmergeMe &src);
+	~PmergeMe(void);
+	PmergeMe &operator=(const PmergeMe &src);
+
+	void			sort(unsigned int pairSize);
+	void			sortPair(int pairSize);
+	void			swapN(unsigned int indexDst, unsigned int indexSrc, unsigned int n);
+	int				calcJacobsthal(unsigned int index);
+	void			printValues(void);
+	void			addRange(std::vector<int>::iterator start, std::vector<int>::iterator end);
+	unsigned int	size();
+};
+
+//Constructor
+template <typename T>
+PmergeMe<T>::PmergeMe(void) {}
+
+template <typename T>
+PmergeMe<T>::PmergeMe(const PmergeMe &src)
+{
+	*this = src;
+}
+
+template <typename T>
+PmergeMe<T>::~PmergeMe(void) {}
+
+template <typename T>
+PmergeMe<T> &PmergeMe<T>::operator=(const PmergeMe &src)
+{
+	if (this != &src)
+		_container = T(src._container);
+	return *this;
+}
+
+//Utils
+template <typename T>
+int PmergeMe<T>::calcJacobsthal(unsigned int index) {
+	if (index <= 1)
+		return (0);
+	if (index == 2)
+		return (1);
+	return (calcJacobsthal(index - 2) * 2 + calcJacobsthal(index - 1));
+}
+
+template <typename T>
+void PmergeMe<T>::swapN(unsigned int indexDst, unsigned int indexSrc, unsigned int n) {
+	int tmp;
+	for (unsigned int i = 0; i < n; i++) {
+		tmp = _container[indexDst + i];
+		_container[indexDst + i] = _container[indexSrc + i];
+		_container[indexSrc + i] = tmp;
+	}
+}
+
+template <typename T>
+void PmergeMe<T>::sortPair(int pairSize)
+{
+	int	containerSize = _container.size();
+	for (int i = 0; i + pairSize * 2 <= containerSize; i += pairSize * 2) {
+		int n1 = _container[i + (pairSize - 1)];
+		int n2 = _container[i + (pairSize * 2 - 1)];
+		if (n1 > n2)
+			swapN(i, i + pairSize, pairSize);
+	}
+
+	if (pairSize * 4 <= containerSize)
+		return (sort(pairSize * 2));
+}
+
+template <typename T>
+void PmergeMe<T>::sort(unsigned int pairSize)
+{
+	sortPair(pairSize);
+
+	// Init pends, same as usual, but we don't delete element because its faster.
+	unsigned int currentPair = 0;
+	unsigned int i = 0;
+	T pend;
+	T nonParticipating;
+	T newContainer;
+
+	unsigned int size = _container.size();
+
+	while (i < size)
+	{
+		if (i + pairSize <= size) {
+			for (unsigned int j = 0; j < pairSize; j++) {
+				if (currentPair % 2 == 0 && currentPair > 1)
+					pend.push_back(_container[i + j]);
+				else
+					nonParticipating.push_back(_container[i + j]);
+			}
+		} else {
+			while (i++ < size - 1)
+				nonParticipating.push_back(_container[i]);;
+		}
+		currentPair++;
+		i += pairSize;
+	}
+
+	_container.swap(newContainer);
 	
+	int jacobIndex = 4;
+
+	while (!pend.empty())
+	{
+		unsigned int pairCount = pend.size() / pairSize;
+		unsigned int jacobDiff = calcJacobsthal(jacobIndex) - calcJacobsthal(jacobIndex - 1);
+		unsigned int pairToInsert = (jacobDiff * (jacobDiff < pairCount)) + (pairCount * (jacobDiff > pairCount)); //replace if else
+
+		for (int pair = pairToInsert; pair > 0; --pair)
+		{
+			T keys;
+			for (unsigned int i = pairSize; i <= _container.size(); i += pairSize)
+				keys.push_back(_container[i - 1]);
+			
+			int value = pend[pairSize * pair - 1];
+			typename T::iterator pos = 	std::lower_bound(keys.begin(), keys.end(), value);
+			unsigned int insertIndex = std::distance(keys.begin(), pos) * pairSize;
+
+			_container.insert(
+				_container.begin() + insertIndex,
+				pend.begin() + pairSize * (pair - 1),
+				pend.begin() + pairSize * pair
+			);
+
+			pend.erase(
+				pend.begin() + pairSize * (pair - 1),
+				pend.begin() + pairSize * pair
+			);
+		}
+
+		jacobIndex++;
+	}
+	
+	for (typename T::iterator it = nonParticipating.begin(); it != nonParticipating.end(); it++)
+		_container.push_back(*it);
+}
+
+template <typename T>
+void PmergeMe<T>::printValues(void)
+{
+	for (typename T::iterator it = _container.begin(); it != _container.end(); ++it)
+		std::cout << *it << " ";
+}
+
+template <typename T>
+void PmergeMe<T>::addRange(std::vector<int>::iterator start, std::vector<int>::iterator end)
+{
+	_container.insert(_container.end(), start, end);
+}
+
+template <typename T>
+unsigned int PmergeMe<T>::size()
+{
+	return _container.size();
 }
 
 #endif
--- cpp/cpp09/ex02/srcs/main.cpp
+++ cpp/cpp09/ex02/srcs/main.cpp
@@ -6,20 +6,90 @@
 /*   By: ntome <ntome@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 14:49:12 by ntome             #+#    #+#             */
-/*   Updated: 2026/03/26 18:04:22 by ntome            ###   ########.fr       */
+/*   Updated: 2026/03/27 11:41:54 by ntome            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RPN.hpp"
+#include "PmergeMe.hpp"
 #include "color.hpp"
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <deque>
+
+bool	vec_has(std::vector<int> vec, int element)
+{
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+		if (*it == element)
+			return (true);
+	}
+	return (false);
+}
+
+std::vector<int>	parse_input(int ac, char **av)
+{
+	std::vector<int>	parsed_input;
+	for (int i = 1; i < ac; i++) {
+		std::string	string_value = av[i];
+		int			int_value;
+		for (int j = 0; j < (int)string_value.length(); j++) {
+			if (!std::isdigit(string_value[j]) && string_value[j] != '+') {
+				if (string_value[j] == '.')
+					throw std::runtime_error(string_value + " is a floating value, we only accept int value.");
+				else if (string_value[j] == '-')
+					throw std::runtime_error(string_value + " is a negative value, please enter only values between 0 and 2147483647.");
+				else
+					throw std::runtime_error(string_value + " is not a numeric value.");
+			}
+		}
+		if (string_value.length() >= 10 && string_value.compare("2147483647") > 0)
+			throw std::runtime_error(string_value + " is greater that the int max, please give only values between 0 and 2147483647.");
+		int_value = std::atoi(av[i]);
+		if (vec_has(parsed_input, int_value))
+			throw std::runtime_error(string_value + " is twice in the inpute, I don't let you do that sry 😝 😝 😝 😝");
+		parsed_input.push_back(int_value);
+	}
+	return (parsed_input);
+}
 
 int main(int ac, char **av)
 {
-	if (ac != 2) {
-		std::cout << RED << "Error: Bad input." << RESET << std::endl;
+	if (ac < 2) {
+		std::cout << RED << "Error: bad input. ./PmergeMe <. . . .>" << RESET << std::endl; 
 		return (1);
 	}
-	RPN	rpn;
-	rpn.calculate(av[1]);
+	std::vector<int>	parsed_val;
+	try {
+		parsed_val = parse_input(ac, av);
+	} catch (std::exception &e) {
+		std::cout << RED << "Error: " << e.what() << std::endl;
+		return (1);
+	}
+
+	PmergeMe<std::vector<int> >	vectorContainer;
+	PmergeMe<std::deque<int> >	dequeContainer;
+	clock_t						time;
+
+	vectorContainer.addRange(parsed_val.begin(), parsed_val.end());
+	dequeContainer.addRange(parsed_val.begin(), parsed_val.end());
+
+	std::cout << YELLOW << "Before: " << RESET;
+	vectorContainer.printValues();
+	std::cout << std::endl;
+
+	time = clock();
+	vectorContainer.sort(1);
+	time = clock() - time;
+
+	std::cout << std::endl << GREEN << "After: " << RESET;
+	vectorContainer.printValues();
+	std::cout << std::endl;
+
+	std::cout << CYAN << "Time to process a range of " << vectorContainer.size() << " elements with std::vector<int> in: " << RESET << ((float)time)/CLOCKS_PER_SEC * 1000 << CYAN << " ms." << RESET << std::endl;
+
+	time = clock();
+	dequeContainer.sort(1);
+	time = clock() - time;
+
+	std::cout << CYAN << "Time to process a range of " << dequeContainer.size() << " elements with std::deque<int> in: " << RESET << ((float)time)/CLOCKS_PER_SEC * 1000 << CYAN << " ms." << RESET << std::endl;
 }
